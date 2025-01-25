a7a4a47a189882409008be4e90b31fe1b809bec0
jollygrin <JollyGrin@protonmail.com>
2025-01-25 19:05:41 +0100

    update configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { loadEnv } from 'vite';
+
+const env = loadEnv(import.meta.env.MODE || 'development', process.cwd(), '');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,7 +17,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.PUBLIC_BASE_PATH || ''
+			base: env.PUBLIC_BASE_PATH || ''
 		}
 	}
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	base: process.env.PUBLIC_BASE_PATH || ''
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		plugins: [sveltekit()],
+		base: env.PUBLIC_BASE_PATH || ''
+	};
 });
